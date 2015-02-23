236fa60414e37cb8c70ae7774f00c6c1f81c42db
Jake Marwil <rjm02006@gmail.com>
2015-02-23 09:53:30 -0700

    change day/night field name to day_night and migrate

--- db/migrate/20150223165042_create_courses.rb
+++ db/migrate/20150223165042_create_courses.rb
@@ -2,7 +2,7 @@ class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
       t.string :title
-      t.boolean :day/night
+      t.boolean :day_night
 
       t.timestamps null: false
     end
